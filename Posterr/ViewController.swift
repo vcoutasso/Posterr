8f3f516a4da73a462b362ed41ca533f2d2d11407
Vinícius Couto Tasso <vcoutasso@gmail.com>
2022-07-31 01:04:56 -0300

    chore: create Xcode project

---
+++ Posterr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posterr
+//
+//  Created by Vinícius Couto on 31/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
